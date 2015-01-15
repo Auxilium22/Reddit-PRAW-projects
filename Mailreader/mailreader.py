12af5859f680c1a0e3ac249e319be57fcad78413
Auxilium <aux.coding@gmail.com>
2015-01-15 20:13:40 +0100

    Revert "New function + first bug fix"
    This reverts commit 087042ee9ce68b338b2ef6a1708f35f0def01808.

--- Mailreader/mailreader.py
+++ Mailreader/mailreader.py
@@ -7,20 +7,8 @@ r = praw.Reddit(user_agent=user_agent)
 # Logging in
 r.login(REDDIT_USERNAME, REDDIT_PASS)
 
-# Making variable to check if we have mail, default is false.
-mail = "false"
- 
-for msg in r.get_unread(limit=None):
-    mail = "true"
-    textfilename = msg.id
- 
-if mail is "false":
-    raise Exception('No Mail')
-    
-# If there is a mail, it writes the content to a new text file with the message id as name, easy to recall it! 
-with open(textfilename +".txt", "w") as f:
+with open("pm.txt", "w") as f:
     for msg in r.get_unread(limit=None):
-        msg2 = msg.body
+        msg2 = str(msg)
         f.write(msg2)
-        print msg.id
         msg.mark_as_read()
