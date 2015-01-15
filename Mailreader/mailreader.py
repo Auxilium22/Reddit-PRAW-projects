93fa50a7995bbbca953c828adc70c13854471ea1
Auxilium <aux.coding@gmail.com>
2015-01-15 16:05:38 +0100

    Mailreader
    Python script, using PRAW to read private message's and store the
    content in the pm.txt file.

---
+++ Mailreader/mailreader.py
@@ -0,0 +1,30 @@
+#import praw
+#from config_bot import *
+#
+#user_agent =  ('PM Reader & Responder 0.01 - by Auxilium11')
+#r = praw.Reddit(user_agent=user_agent)
+#
+# Logging in
+#r.login(REDDIT_USERNAME, REDDIT_PASS)
+#
+#with open("pm.txt", "w") as f:
+#    for msg in r.get_unread(limit=None):
+#        print msg
+#        msg2 = str(msg)
+#        f.write(msg2)
+#        msg.mark_as_read()
+
+import praw
+from config_bot import *
+
+user_agent =  ('PM Reader & Responder 0.1 - by Auxilium11')
+r = praw.Reddit(user_agent=user_agent)
+
+# Logging in
+r.login(REDDIT_USERNAME, REDDIT_PASS)
+
+with open("pm.txt", "w") as f:
+    for msg in r.get_unread(limit=None):
+        msg2 = str(msg)
+        f.write(msg2)
+        msg.mark_as_read()
